5a960249c57de6463a6e759111d67e970c792610
Khanh dep trai <ijuinenzan@yahoo.com>
2017-10-02 22:49:59 +0700

    Edit Width Height and delete Old xml

--- MapEditor/Map.cs
+++ MapEditor/Map.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 public class Map
 {
-    private int _width;
-    private int _height;
+    private int _columns;
+    private int _rows;
     private int _tileWidth;
     private int _tileHeight;
 
@@ -22,27 +22,27 @@ public class Map
         }
     }
 
-    public int Width
+    public int Columns
     {
         get
         {
-            return _width;
+            return _columns;
         }
         set
         {
-            _width = value;
+            _columns = value;
         }
     }
 
-    public int Height
+    public int Rows
     {
         get
         {
-            return _height;
+            return _rows;
         }
         set
         {
-            _height = value;
+            _rows = value;
         }
     }
 
